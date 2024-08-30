a7181612684787778a4fe031a9673c85c33087bf
juligaleanop <juligaleanop@gmail.com>
2024-08-29 22:02:11 -0300

    telescope ya no busca archivos ocultos. no se en que estaba pensando

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,8 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep', 'nvim-treesitter/nvim-treesitter' },
 	opts = function()
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, {noremap = true}) -- en caso de querer buscar dotfiles usar la linea de abajo en lugar de esta
+		--vim.keymap.set('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {noremap = true})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {noremap = true})
 		vim.keymap.set('n', '<leader>ft', builtin.treesitter, {noremap = true})
